ffc099ccf125a6ddaa41b4f4d8ff5a238074830c
sujitgawas19 <sujit@ajency.in>
2017-08-02 12:51:28 +0530

    login

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -87,9 +87,9 @@ export class HomePage {
                   console.log(this);
        // *  Sign in the user upon button click.
 
-        	        gapi.auth2.getAuthInstance().signIn().then( () => {
+        	        gapi.auth2.getAuthInstance().signIn()
                 this.navCtrl.push('SearchPage');
-              });
+            
 
 
           // authorizeButton.style.display = 'block';
